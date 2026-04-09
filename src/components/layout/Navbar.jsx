6361b8b5f894490dd1b96de79862da34cd122684
danah04 <danahaldosary1@gmail.com>
2026-04-09 16:51:49 +0300

    added navbar

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav style={{ padding: "1rem", borderBottom: "1px solid #ddd" }}>
+      <h2>KFUPM Lost & Found</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
