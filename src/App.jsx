3b15cf0e8b8d660aac2133ad8fea9265751fec39
LenaYassserq <s202268180@kfupm.edu.sa>
2026-04-11 17:51:14 +0300

    adding the login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 // Public Pages
 import LandingPage from "./pages/public/LandingPage";
+import LoginPage from "./pages/public/LoginPage"; 
 import AccessDeniedPage from "./pages/public/AccessDeniedPage";
 import AuthFailedPage from "./pages/public/AuthFailedPage";
 
@@ -11,17 +12,16 @@ import FinderDashboard from "./pages/finder/FinderDashboard";
 export default function App() {
   return (
     <Routes>
-
-      
       <Route path="/" element={<LandingPage />} />
 
       
+      <Route path="/login" element={<LoginPage />} />
+
       <Route path="/access-denied" element={<AccessDeniedPage />} />
       <Route path="/auth-failed" element={<AuthFailedPage />} />
 
       {/* Finder Dashboard */}
       <Route path="/finder/dashboard" element={<FinderDashboard />} />
-
     </Routes>
   );
 }
